d8cfca79c89bda374eb49cddbbd7d447e133f9ac
久保田将章 <masaaki@kubotamasaakis-MacBook-Pro.local>
2025-08-14 10:53:04 +0900

    build: Add dist directory for deployment

--- src/components/Input.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export default function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input className={['input', props.className||''].join(' ')} {...props} />
-}
